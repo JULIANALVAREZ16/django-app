b4b413146bc07755b6b3b29d0c9eadae33ad97d4
Julian <julianalvarez199423@gmail.com>
2024-03-11 11:16:33 -0500

    delete

--- appWeb/academics/models.py
+++ appWeb/academics/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class User(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField (max_length= 20)
     age = models.IntegerField()
+    ident_number = models.CharField(max_length=12,blank = True)
+    updated_at = models.DateTimeField(default = datetime.datetime.now())
     
