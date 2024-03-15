8a6c4bd52da883d8d0be1a9b1637d46d7ee09aca
Julian <julianalvarez199423@gmail.com>
2024-03-15 17:17:02 -0500

    insertar datos

--- academics/models.py
+++ academics/models.py
@@ -1,11 +1,19 @@
-from django.db import models
 import datetime
+from django.db import models
 
 # Create your models here.
+# model1.DateTimeField(auto_now = True, null = false)
+
 class User(models.Model):
-    firstname = models.CharField(max_length=20)
-    lastname = models.CharField (max_length= 20)
+    email = models.EmailField(null = True, blank = True)
+    password = models.CharField(null = True, blank = True)
+    status = models.BooleanField(default = True)
+
+class Person(models.Model):
+    firstname = models.CharField(max_length = 20)
+    lastname = models.CharField(max_length = 20)
     age = models.IntegerField()
-    ident_number = models.CharField(max_length=12,blank = True)
-    updated_at = models.DateTimeField(default = datetime.datetime.now())
-    
+    ident_number = models.CharField(max_length = 12, blank = True)
+    create_at = models.DateTimeField(default = datetime.datetime.now())
+    update_at = models.DateTimeField(default = datetime.datetime.now())
+    deleted_at = models.DateTimeField(null = True, blank = True)
